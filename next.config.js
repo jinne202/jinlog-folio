5bab0de1bf4889e975528106d986b7bb0b2bbd53
eunjin <hanb5468@gmail.com>
2021-05-15 19:37:09 +0900

    netlify-deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withImages = require('next-images');
 
 module.exports = withImages({
+    distDir: '.next',
     webpack(config, options) {
         config.module.rules.push({
             test: /\.(ogg|mp3|wav|mpe?g)$/i,
