b9af3fdd89002923dd3c10c18fc415b7c5870d40
eunjin <hanb5468@gmail.com>
2021-05-18 15:55:48 +0900

    meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps /*, AppContext */ } from 'next/app.js';
 import React, { useEffect } from 'react';
+import Head from 'next/head';
 import GlobalStyles from '../styles/global-styles';
 import wrapper from '../store/configureStore';
 import { ThemeProvider } from 'styled-components';
@@ -14,6 +15,16 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     return (
         <>
+            <Head>
+				<title>jin.log</title>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://jin-log.today"/>
+                <meta property="og:title" content="jin.log"/>
+                <meta property="og:description" content="Junoir Front-end Developer Han eunjin Portfolio"/>
+                <meta property="og:image" content="../static/image/thumbnail.png" />
+                <meta property="og:image:width" content="600"/>
+                <meta property="og:image:height" content="400"/>
+			</Head>
             <ThemeProvider theme={theme}>
                 <GlobalStyles />
                 <Component {...pageProps} />
